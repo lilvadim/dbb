3c2c6ff5081ace7a9bf95f10ca82198ce3f49e43
lilvadim <vadimmos2002@gmail.com>
2022-12-14 12:57:09 +0700

    file chooser added

--- src/main/kotlin/ru/nsu/dbb/view/CreateDatabaseScreenController.kt
+++ src/main/kotlin/ru/nsu/dbb/view/CreateDatabaseScreenController.kt
@@ -1,10 +1,17 @@
 package ru.nsu.dbb.view
 
 import javafx.fxml.FXML
+import javafx.stage.FileChooser
 import javafx.stage.Stage
 import ru.nucodelabs.kfx.core.AbstractController
+import java.io.File
 
 class CreateDatabaseScreenController : AbstractController() {
+    fun chooseFile() {
+        val fileChooser = FileChooser()
+        val chosenFile: File? = fileChooser.showOpenDialog(stage)
+    }
+
     @FXML
     private lateinit var root: Stage
     override val stage: Stage
