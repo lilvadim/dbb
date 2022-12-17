7127bf9843fb8c4cf3d0785c0e5561d58c0d4a23
DevKhaverko <v.khaverko@tinkoff.ru>
2022-12-17 14:05:26 +0700

    Added queries for indecies and tests for constructor

--- src/main/java/ru/nsu/dbb/constructor/QueryConstructor.java
+++ src/main/java/ru/nsu/dbb/constructor/QueryConstructor.java
@@ -1,11 +1,17 @@
 package ru.nsu.dbb.constructor;
 
+import java.util.ArrayList;
+
 public class QueryConstructor {
     private static final String addColumnStatementTemplate = "ALTER TABLE %s ADD COLUMN %s %s %s";
 
     private static final String dropColumnStatementTemplate = "ALTER TABLE %s DROP COLUMN %s";
 
     private static final String renameColumnStatementTemplate = "ALTER TABLE %s RENAME %s TO %s";
+
+    private static final String createIndexStatementTemplate  = "CREATE INDEX %s ON %s (%s)";
+
+    private static final String dropIndexStatementTemplate = "DROP INDEX %s";
 
     // todo сделать для других бд, так как sqlite не поддерживает изменение типа колонки
     private static final String changeColumnDataTypeTemplate = "";
@@ -32,5 +38,28 @@ public class QueryConstructor {
         );
     }
 
+    public String createIndexStatement(String tableName, String indexName, ArrayList<String> columnsInIndex) {
+        StringBuilder columns = new StringBuilder("");
+        var size = columnsInIndex.size();
+        for (int i = 0; i < size; i++) {
+            if (i != size - 1) {
+                columns.append(columnsInIndex.get(i)).append(",");
+            } else {
+                columns.append(columnsInIndex.get(i));
+            }
+        }
+        return String.format(
+                createIndexStatementTemplate,
+                indexName,
+                tableName,
+                columns
+        );
+    }
 
+    public String createDropIndexStatement(String indexName) {
+        return String.format(
+                dropIndexStatementTemplate,
+                indexName
+        );
+    }
 }
