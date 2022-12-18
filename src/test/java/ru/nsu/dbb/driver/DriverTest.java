c4e366bff067caaf4ac9c04a91a3fb4730d1234b
lilvadim <vadimmos2002@gmail.com>
2022-12-19 02:48:15 +0700

    trying to fix tests

--- src/test/java/ru/nsu/dbb/driver/DriverTest.java
+++ src/test/java/ru/nsu/dbb/driver/DriverTest.java
@@ -15,7 +15,7 @@ class DriverTest {
     @Test
     void getDatabaseMeta() throws SQLException {
         Driver driver = new Driver();
-        driver.openConnection("jdbc:sqlite:test.db", "", "");
+        driver.openConnection("jdbc:sqlite:src/test/resources/test.db", "", "");
         Database actualDatabase = driver.getDatabaseMeta();
         var expected = new Database();
         ArrayList<Column> columns = new ArrayList<>(Arrays.asList(
