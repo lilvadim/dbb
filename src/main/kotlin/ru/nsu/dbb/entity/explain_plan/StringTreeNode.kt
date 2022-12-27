d6adcaaeb636d8e8ecd236b1d533f19962752ad8
lilvadim <vadimmos2002@gmail.com>
2022-12-28 01:48:52 +0700

    update entity

--- src/main/kotlin/ru/nsu/dbb/entity/explain_plan/StringTreeNode.kt
+++ src/main/kotlin/ru/nsu/dbb/entity/explain_plan/StringTreeNode.kt
@@ -1,3 +1,9 @@
 package ru.nsu.dbb.entity.explain_plan
 
-data class StringTreeNode (val columns: List<String>, val children: MutableList<StringTreeNode> = mutableListOf(), var parent: StringTreeNode? = null)
+data class StringTreeNode(
+    /**
+     * Mapping column name -> value
+     */
+    val columns: Map<String, String>,
+    val children: List<StringTreeNode>
+)
